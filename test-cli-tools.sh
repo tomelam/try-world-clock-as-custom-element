1883bb2bb71438ebbaa8ca7ee985b8baa2f48a2f
Tom Elam <tomelam@gmail.com>
2020-09-06 16:27:04 +0530

    Checked initialization of the project by checking the existence of the package.json file.

--- test-cli-tools.sh
+++ test-cli-tools.sh
@@ -7,14 +7,20 @@ if [[ -z "$(node --version)" ]] ; then
   exit 1
 fi
 
+if [[ ! -f package.json ]] ; then
+  echo "Initialize the project first:"
+  echo "npm init --yes"
+  exit 2
+fi
+
 if ! npm list @dojo/cli > /dev/null ; then
   echo "@dojo/cli not installed locally. Please install it first:"
   echo "npm i @dojo/cli"
-  exit 2
+  exit 3
 fi
 
 if ! npm list @dojo/cli-build-widget > /dev/null ; then
   echo "@dojo/cli-build-widget not installed locally. Please install it first:"
   echo "npm i @dojo/cli-build-widget"
-  exit 3
+  exit 4
 fi
