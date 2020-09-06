d7bf37be0f626963bc2c0e3ca505d91ab2c5ba97
Tom Elam <tomelam@gmail.com>
2020-09-06 23:23:31 +0530

    Only check for Node.

--- test-cli-tools.sh
+++ test-cli-tools.sh
@@ -2,20 +2,25 @@
 
 echo "Checking Dojo CLI tools for compiling a widget to a custom element ..."
 
+if [[ -z "$(node --version)" ]] ; then
+  echo "Install Node first."
+  exit 1
+fi
+
 if [[ ! -f package.json ]] ; then
   echo "Initialize the project first:"
   echo "npm init --yes"
-  exit 3
+  exit 2
 fi
 
 if ! npm list @dojo/cli > /dev/null ; then
   echo "@dojo/cli not installed locally. Please install it first:"
   echo "npm i @dojo/cli"
-  exit 4
+  exit 3
 fi
 
 if ! npm list @dojo/cli-build-widget > /dev/null ; then
   echo "@dojo/cli-build-widget not installed locally. Please install it first:"
   echo "npm i @dojo/cli-build-widget"
-  exit 5
+  exit 4
 fi
