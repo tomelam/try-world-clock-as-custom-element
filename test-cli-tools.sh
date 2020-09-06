6397df984f037650c359bbe29ebaa44ffd1a807d
Tom Elam <tomelam@gmail.com>
2020-09-06 20:57:40 +0530

    Removed bad test from test-cli-tools.sh .

--- test-cli-tools.sh
+++ test-cli-tools.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-function version { echo "$@" | \
-                   awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-
 echo "Checking Dojo CLI tools for compiling a widget to a custom element ..."
-
-NODE=$(which node)
-if [[ -z "$NODE" ]] ; then
-  echo "Node is not installed. Install it first."
-  exit 1
-fi
-
-export NODE_VERSION=$(node --version)
-echo NODE_VERSION is $NODE_VERSION
-if [[ $(version "v14.0.0") -ge $(version $NODE_VERSION) ]] ; then
-  echo "Node is older than v14.0.0. Update Node first."
-  exit 2
-fi
 
 if [[ ! -f package.json ]] ; then
   echo "Initialize the project first:"
